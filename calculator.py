858bf622ee0c23618ae4ef8bef2f72efb8294a13
AdamSmithHH <89928453+AdamSmithHH@users.noreply.github.com>
2021-11-18 17:26:41 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,75 @@
+banner = '''
+                                                                                                   
+ @@@@@@@   @@@@@@   @@@        @@@@@@@  @@@  @@@  @@@        @@@@@@   @@@@@@@   @@@@@@   @@@@@@@   
+@@@@@@@@  @@@@@@@@  @@@       @@@@@@@@  @@@  @@@  @@@       @@@@@@@@  @@@@@@@  @@@@@@@@  @@@@@@@@  
+!@@       @@!  @@@  @@!       !@@       @@!  @@@  @@!       @@!  @@@    @@!    @@!  @@@  @@!  @@@  
+!@!       !@!  @!@  !@!       !@!       !@!  @!@  !@!       !@!  @!@    !@!    !@!  @!@  !@!  @!@  
+!@!       @!@!@!@!  @!!       !@!       @!@  !@!  @!!       @!@!@!@!    @!!    @!@  !@!  @!@!!@!   
+!!!       !!!@!!!!  !!!       !!!       !@!  !!!  !!!       !!!@!!!!    !!!    !@!  !!!  !!@!@!    
+:!!       !!:  !!!  !!:       :!!       !!:  !!!  !!:       !!:  !!!    !!:    !!:  !!!  !!: :!!   
+:!:       :!:  !:!   :!:      :!:       :!:  !:!   :!:      :!:  !:!    :!:    :!:  !:!  :!:  !:!  
+ ::: :::  ::   :::   :: ::::   ::: :::  ::::: ::   :: ::::  ::   :::     ::    ::::: ::  ::   :::  
+ :: :: :   :   : :  : :: : :   :: :: :   : :  :   : :: : :   :   : :     :      : :  :    :   : :  
+                                                                                                   
+'''
+print(banner)
+
+while True:
+    islem=input("Please Select Transaction(+,-,*,/,**) : ")
+    if(islem=="q"):
+        print("EXİT")
+        break
+    elif(islem=="+"):
+        sayi1=int(input("Number one : "))
+        sayi2=int(input("Number two : "))
+        try:
+            sayi_1 = int(sayi1)
+            sayi_2 = int(sayi2)
+            total = sayi_1 + sayi_2
+            print(total)
+        except ValueError:
+            print("Please use the number !!!!")
+    elif(islem=="-"):
+        sayi1=input("Number one : ")
+        sayi2=input("Number two : ")
+        try:
+            sayi_1 = int(sayi1)
+            sayi_2 = int(sayi2)
+            total = sayi_1 - sayi_2
+            print(total)
+        except ValueError:
+            print("Please use the number !!!!")
+    elif(islem=="*"):
+        sayi1=input("Number one : ")
+        sayi2=input("Number two : ")
+        try:
+            sayi_1 = int(sayi1)
+            sayi_2 = int(sayi2)
+            total = sayi_1 * sayi_2
+            print(total)
+        except ValueError:
+            print("Please use the number !!!!")
+    elif(islem=="/"):
+        sayi1=input("Number one : ")
+        sayi2=input("Number two : ")
+        try:
+            sayi_1 = int(sayi1)
+            sayi_2 = int(sayi2)
+            total = sayi_1 / sayi_2
+            print(total)
+        except ValueError:
+            print("Please use the number !!!!")
+    elif(islem=="**"):
+        sayi1=input("Number one : ")
+        sayi2=input("Number two : ")
+        try:
+            sayi_1 = int(sayi1)
+            sayi_2 = int(sayi2)
+            total = sayi_1 ** sayi_2
+            print(total)
+        except ValueError:
+            print("Please use the number !!!!")
+    
+
+
+
